1842466de65a9a01e0de1546b9e9170ff20d8359
Gustavo de Almeida Santos <gustavo@worker.worker.com>
2019-10-14 23:44:22 -0300

    go...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'roboto-fontface';
+import { AppBar, Toolbar, Typography, Paper, Card, Grid } from '@material-ui/core'
+import Box from '@material-ui/core/Box';
 
 function App() {
+
+  const paper = { marginTop: 40, width: '80%', padding: 20 };
+  const card = { width: 150 };
+
+  //{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(value => (
+  /*
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+            13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+            25
+  */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AppBar position="static" style={{ marginBottom: 15 }}>
+        <Toolbar>
+          <Typography style={{ flexGrow: 1 }} variant="h4">
+            Gustavo de Almeida Santos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Grid container justify="center">
+        <Grid item xs={10}>
+
+          <Grid container spacing={6}>
+            <Grid item xs={12} sm={12} md={12}>
+              <Paper style={{ padding: 5 }}>
+                <Box display="flex" justifyContent="center">
+                  <Typography>
+                    Gustavo de Almeida Santos | xs={12} sm={12} md={12}
+                  </Typography>
+                </Box>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <Paper style={{ padding: 5 }}>xs={12} sm={12} md={6}</Paper>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <Paper style={{ padding: 5 }}>xs={12} sm={12} md={6}</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Paper style={{ padding: 5 }}>xs={12} sm={6} md={4}</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Paper style={{ padding: 5 }}>xs={12} sm={6} md={4}</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Paper style={{ padding: 5 }}>xs={12} sm={6} md={4}</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Paper style={{ padding: 5 }}>xs={12} sm={6} md={4}</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Paper style={{ padding: 5 }}>xs={12} sm={6} md={4}</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Paper style={{ padding: 5 }}>xs={12} sm={6} md={4}</Paper>
+            </Grid>
+          </Grid>
+
+        </Grid>
+      </Grid>
+
+    </React.Fragment>
   );
 }
 

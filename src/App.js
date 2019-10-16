134c60a70a1af4aa0c6fc5b0a36314c499597768
Gustavo de Almeida Santos <gustavo@worker.worker.com>
2019-10-16 20:33:59 -0300

    going on...

--- src/App.js
+++ src/App.js
@@ -1,43 +1,120 @@
 import React from 'react';
-import { Typography, Card, Grid, Link } from '@material-ui/core'
+import {
+  Typography
+  , Card
+  , Grid
+  , Link
+  , Box
+  , Avatar
+  /*,Paper
+  ,CardActionArea
+  ,CardActions
+  ,CardContent
+  ,CardMedia*/
+} from '@material-ui/core'
+import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+import BarChartRounded from '@material-ui/icons/BarChartRounded';
+import TrendingUpRoundedIcon from '@material-ui/icons/TrendingUpRounded';
+import BusinessCenterRoundedIcon from '@material-ui/icons/BusinessCenterRounded';
 
 function App() {
 
-  const externalGrid = { marginTop: 40 }
-  const card = { width: 300 }
+  const externalDiv = { marginTop: 50 }
+  const card = {}
   const link = { textDecoration: "none" }
 
   return (
     <React.Fragment>
+      <div >
+        <Grid container style={externalDiv} justify="center">
+          <Grid item xs={12} sm={11} md={10}>
+            <Grid container
+              direction="row"
+              justify="center"
+              alignItems="flex-start"
+              spacing={2}>
 
-      <Grid container style={externalGrid} justify="center">
-        <Grid item xs={10}>
+              <Grid item xs={12} sm={4} md={3}>
+                <Link href="/other" style={link}>
+                  <Card style={card}>
+                    <Box display="flex" p={2}>
+                      <Box p={1} >
+                        <Avatar>
+                          <PeopleAltRoundedIcon />
+                        </Avatar>
+                      </Box>
+                      <Box p={1}>
+                        <Typography gutterBottom variant="h6">
+                          Customers
+                    </Typography>
+                      </Box>
+                    </Box>
+                  </Card>
+                </Link>
+              </Grid>
 
-          <Grid container justify="center" padding={2}>
+              <Grid item xs={12} sm={4} md={3}>
+                <Link href="/other" style={link}>
+                  <Card style={card}>
+                    <Box display="flex" p={2}>
+                      <Box p={1} >
+                        <Avatar>
+                          <BusinessCenterRoundedIcon />
+                        </Avatar>
+                      </Box>
+                      <Box p={1}>
+                        <Typography gutterBottom variant="h6">
+                          Enterprises
+                    </Typography>
+                      </Box>
+                    </Box>
+                  </Card>
+                </Link>
+              </Grid>
 
-            <Grid item xs={6} sm={4} md={3}>
-              <Link href="/other" style={link}>
-                <Card style={card}>
-                  <Typography variant="h4">
-                    Other
-                </Typography>
-                </Card>
-              </Link>
+              <Grid item xs={12} sm={4} md={3}>
+                <Link href="/other" style={link}>
+                  <Card style={card}>
+                    <Box display="flex" p={2}>
+                      <Box p={1} >
+                        <Avatar>
+                          <TrendingUpRoundedIcon />
+                        </Avatar>
+                      </Box>
+                      <Box p={1}>
+                        <Typography gutterBottom variant="h6">
+                          Sales
+                    </Typography>
+                      </Box>
+                    </Box>
+                  </Card>
+                </Link>
+              </Grid>
+
+              <Grid item xs={12} sm={4} md={3}>
+                <Link href="/other" style={link}>
+                  <Card style={card}>
+                    <Box display="flex" p={2}>
+                      <Box p={1} >
+                        <Avatar>
+                          <BarChartRounded fontSize="large" />
+                        </Avatar>
+                      </Box>
+                      <Box p={1}>
+                        <Typography gutterBottom variant="h6">
+                          Statistics
+                    </Typography>
+                      </Box>
+                    </Box>
+                  </Card>
+                </Link>
+              </Grid>
+
             </Grid>
-
-            <Grid item xs={6} sm={4} md={3}>
-              <Card style={card}>
-                <Typography variant="h4">
-                  Other
-                </Typography>
-              </Card>
-            </Grid>
-
           </Grid>
-
         </Grid>
-      </Grid>
 
+      </div>
     </React.Fragment>
   );
 }
